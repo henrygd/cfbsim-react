a195900a21750fe8ae88e6b153f0494a4113597b
hank <hank@henrygd.me>
2016-11-14 11:16:32 -0500

    Update ratings -- Mon Nov 14 11:16:32 EST 2016

--- custom_scripts/update.sh
+++ custom_scripts/update.sh
@@ -2,7 +2,7 @@
 
 # will break if cfbstats.com changes current site format
 
-rm -rf national_stats team_pages
+rm -rf national_stats/* team_pages/*
 ./grab_teams.rb
 ./grab_national_stats.rb
 ./save_ratings.rb
--- src/index.js
+++ src/index.js
@@ -41,109 +41,37 @@ function run() {
 }
 
 function _init() {
-	// var teamOne, teamTwo;
-	// const path = window.location.pathname;
 	rootElement.className = 'scanlines';
-	// if (path === '/') {
-	// 	teamOne = randomTeam()[0];
-	// 	teamTwo = randomTeam()[0];
-	// 	browserHistory.replace(`/${teamOne}/${teamTwo}`);
-	// }
 	run();
 }
 
-
-
-function fetchData(url, method, cb) {
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, url, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			var status = xhr.status;
-			if (status === 200) {
-				// success
-				cb(xhr.responseText);
+// fetch updated team ratings & (if needed) store logos / colors
+fetch(`/js/teamratings.json?${+new Date()}`).then(r => r.json())
+  .then(data => {
+		// grabbed teams successfully
+		window.teamRatings = data[0];
+		window.lastUpdate = data[1];
+		window.teamKeys = Object.keys(data[0]);
+		// check if team logos are pre-cached in localStorage
+		if (!localStorage) {
+			return alert('To use this website, please download a browser that supports local storage.');
+		} else {
+			if (localStorage.color_Wild) {
+				// already have logos, go on with loading the page
+				setTimeout(_init, 500);
 			} else {
-				alert('Error: Failed to load needed assets');
+				// get logos
+				fetch('/js/localStoreTeams.json').then(r => r.json())
+					.then(teamData => {
+						console.log(teamData);
+						teamData.forEach(teamArr => {
+							var teamName = teamArr[0];
+							localStorage['logo_' + teamName] = teamArr[1];
+							localStorage['color_' + teamName] = teamArr[2];
+						});
+						setTimeout(_init, 500);
+					})
 			}
 		}
-	};
-	xhr.send();
-}
-
-// get team rankings & (if needed) logos
-fetchData(`/js/teamratings.json?${+new Date()}`, 'GET', function(data) {
-	var teams;
-	try {
-		teams = JSON.parse(data);
-	} catch(err) {
-		return alert ('Error: Could not fetch team rankings.');
-	}
-	// grabbed teams successfully
-	window.teamRatings = teams[0];
-	window.lastUpdate  = teams[1];
-	window.teamKeys = Object.keys(teams[0]);
-	// check if team logos are pre-cached in localStorage
-	if (localStorage !== null) {
-		if (localStorage.color_LSU) {
-			// go on with loading the page
-			setTimeout(_init, 500);
-		} else {
-			// get logos
-			window.processLocalStore = function(data) {
-				data.forEach(function(arr) {
-					localStorage['logo_'+arr[0]]=arr[1];
-					localStorage['color_'+arr[0]]=arr[2];
-				});
-				setTimeout(_init, 500);
-			};
-			var tag = document.createElement('script');
-			tag.src = '/js/localStoreTeams.js';
-			document.body.appendChild(tag);
-		}
-	} else {
-		// if no local storage alert error
-		alert('To use this website, please download a browser that supports local storage.');
-	}
-});
-
-// es6 async scratched due to extra 2kb build size
-// // async function to grab team ratings
-// async function fetchJSON(url, errMsg, cb) {
-//   try {
-//     const response = await fetch(url);
-//     cb(await response.json());
-//   }
-//   catch (err) {
-//     alert(errMsg);
-//     console.log(err);
-//   }
-// }
-
-// // grab team ratings / check local storage for team info
-// fetchJSON(`/js/teamratings.json?${+new Date()}`, 'Error: Failed to fetch teams', (data) => {
-// 	// grabbed teams successfully
-// 	window.teamRatings = data;
-// 	// check if team logos are pre-cached in localStorage
-// 	if (localStorage !== null) {
-// 		if (localStorage.color_Michigan) {
-// 			// go on with loading the page
-// 			setTimeout(_init, 500);
-// 		} else {
-// 			// get logos
-// 			window.processLocalStore = function(data) {
-// 				data.forEach(function(arr) {
-// 					localStorage['logo_'+arr[0]]=arr[1];
-// 					localStorage['color_'+arr[0]]=arr[2];
-// 				});
-// 				setTimeout(_init, 500);
-// 			};
-// 			var tag = document.createElement('script');
-// 			tag.src = '/js/localStoreTeams.js';
-// 			document.body.appendChild(tag);
-// 		}
-// 	} else {
-// 		// if no local storage alert error
-// 		alert('To use this website, please download a browser that supports local storage.');
-// 	}
-// });
+  })
+  .catch(e => alert('Error: Could not fetch team rankings.'))
